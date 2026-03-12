36090fa593472b2ae4fcc8ded6e72d34325c18d9
Pattles <doomrock8@gmail.com>
2026-03-12 15:02:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+*Last modified: March 12 2026*
+
+This exists now..
+
+Before you start anything, check:
+> Is nodemon installed? 
+- npm install -g nodemon
+
+> Is nodejs installed? 
+- Install it yourself somewhere
+
+
+To start localhost:
+- open the project in Terminal
+- run 'nodemon server.js'
+- open http://localhost:3000/static_homepage.html
+You're done.
+
+Try to break it? Work on improving it? Up to you..
+Hopefully I wrote enough comments for it not to be a nightmare to dissect later.
+
+All the server.js stuff is written by Claude, so good luck with that if you ever decide to change anything.
+Most of the HTML and CSS is written by yours truly, with some advice-garnering from Claude.
+
+Even with that, I spent 5 hours from start to finish on this project. Was it worth it? Probably not.
---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    
+    // Serve the HTML page
+    if (req.url === '/' || req.url === '/static_homepage.html') {
+        const file = fs.readFileSync(path.join(__dirname, 'static_homepage.html'));
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(file);
+    }
+
+    // Save a new event
+    else if (req.url === '/add-event' && req.method === 'POST') {
+        console.log('received request')
+
+        let body = '';
+        req.on('data', chunk => body += chunk);
+        req.on('end', () => {
+            console.log('body:', body);
+
+            const newEvent = JSON.parse(body);
+            const events = JSON.parse(fs.readFileSync('events.json'));
+            events.push(newEvent);
+            fs.writeFileSync('events.json', JSON.stringify(events, null, 2));
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ success: true }));
+        });
+    }
+    // Read events.json
+    else if (req.url === '/get-events' && req.method === 'GET') {
+        const events = JSON.parse(fs.readFileSync('events.json'));
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(events));
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
